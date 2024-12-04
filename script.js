613c57f7a93fcb2f3af7a5cd565328766e1657d9
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-12-04 13:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,31 +2,34 @@
 let Promise1=new Promise((resolve,reject)=>
 	{
 		let s1=parseInt(((Math.random()*2+1))*1000);
-		setTimeout(()=>{
+		setTimeout(()=>
 		
-			resolve(["promise 1 resolved",s1]},
+			resolve(["promise 1 resolved",s1]
+                ),
 				 s1)
 	})
 
 let Promise2=new Promise((resolve,reject)=>
 	{
 		let s2=parseInt(((Math.random()*2+1))*1000);
-		setTimeout(()=>{
-           resolve(["promise 2 resolved",s2]},
+		setTimeout(()=>
+           resolve(["promise 2 resolved",s2]),
 				 s2)
 	})
 
 let Promise3=new Promise((resolve,reject)=>
 	{
 		let s3=parseInt(((Math.random()*2+1))*1000);
-		setTimeout(()=>{
-           resolve(["promise 3 resolved",s3]},
+		setTimeout(()=>
+           resolve(["promise 3 resolved",s3]),
 				 s3)
+        
 	})
 
 
 Promise.all([Promise1,Promise2,Promise3]).then((data)=>
 	{
+        let t1=new Date().getMilliseconds();
 		let tb=document.getElementById('output');
 		tb.innerHTML="";
 
@@ -50,8 +53,13 @@ Promise.all([Promise1,Promise2,Promise3]).then((data)=>
 		let d1=document.createElement('td');
 		let d2=document.createElement('td');
 
+        let t2=new Date().getMilliseconds();
+
 		d1.textContent="Total";
-		d2.textContent = ((data[0][1] + data[1][1] + data[2][1]) / 1000).toFixed(3);
+
+        let ans=Math.max(data[0][1],data[1][1],data[2][1]); 
+
+		d2.textContent = (ans/1000).toFixed(3);
 
 
 		r1.appendChild(d1);
